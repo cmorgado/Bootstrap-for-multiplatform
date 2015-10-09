47e51b8beed81bb0a92e07863c5397d126a16cff
Cristovão Morgado <cm_morgado@hotmail.com>
2015-10-09 15:27:03 +0100

    Fixed XForms compilation issues

--- Bootstrap/Bootstrap.Droid/Resources/Resource.Designer.cs
+++ Bootstrap/Bootstrap.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace Bootstrap.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::GalaSoft.MvvmLight.Resource.String.ApplicationName = global::Bootstrap.Droid.Resource.String.ApplicationName;
+			global::GalaSoft.MvvmLight.Resource.String.Hello = global::Bootstrap.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
@@ -53,6 +55,25 @@ namespace Bootstrap.Droid
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f030001
+			public const int ApplicationName = 2130903041;
+			
+			// aapt resource value: 0x7f030000
+			public const int Hello = 2130903040;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
 			{
 			}
 		}
