b45d70c2963abb75c148ba23fe38b154d947c29b
Cristovão Morgado <cm_morgado@hotmail.com>
2015-10-12 15:08:52 +0100

    missing file

---
+++ Bootstrap/Bootstrap.Droid/Service/PlatformService.cs
@@ -0,0 +1,92 @@
+﻿
+using Bootstrap.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootstrap.PlatformSpecific.Services
+{
+    public class PlatformService : Bootstrap.Core.Interfaces.IPlatform
+    {
+        public string AppName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string appVersion
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string deviceID
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string deviceName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string OSName
+        {
+            get
+            {
+                return "Droid";
+            }
+        }
+
+        public string OSVersion
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string xAuthKey
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string xAuthUser
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
